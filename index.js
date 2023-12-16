949cc30ba553f6d879bff16f1fdb195396138036
sein <seinkane01@gmail.com>
2023-12-16 13:37:54 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function countDown(startingNumber) {
+if (typeof startingNumber !== 'number' || startingNumber < 0 || !Number.isInteger(startingNumber)) {
+      console.error('Please provide a valid positive integer for counting down.');
+      return;
+    }
+   while (startingNumber >= 0) {
+      console.log(startingNumber);
+      startingNumber--;
+    }
+  }
+  
+  
+  
+``
